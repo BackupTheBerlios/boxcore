c72e7f19e11bda2f050afd8fdff2ffb1e1775551
Carsomyr <devnull@localhost>
2008-12-25 23:01:56 +0200

    Remove errors and fix formatting of the trace code

--- debug/debug.h
+++ debug/debug.h
@@ -10,7 +10,7 @@
 void trace_printf(const char *pFileName, unsigned int pLine, const char *pFunction, const char *pFormat, ...);
 
 #ifndef NDEBUG
-#define TRACE(...) trace_printf(__FILE__, __LINE__, __PRETTY_FUNCTION, __VA_ARGS__)
+#define TRACE(...) trace_printf(__FILE__, __LINE__, __PRETTY_FUNCTION__, __VA_ARGS__)
 #else
 #define TRACE(...)
 #endif //NDEBUG
