5c8e56652ca6acba2316b998f6613654191fee59
Carsomyr <devnull@localhost>
2008-12-29 23:36:48 +0200

    Added a PRINT statement to debug.h

--- debug/debug.h
+++ debug/debug.h
@@ -10,8 +10,10 @@
 void trace_printf(const char *pFileName, unsigned int pLine, const char *pFunction, const char *pFormat, ...);
 
 #ifndef NDEBUG
+#define PRINT(A) OutputDebugString(A)
 #define TRACE(...) trace_printf(__FILE__, __LINE__, __PRETTY_FUNCTION__, __VA_ARGS__)
 #else
+#define PRINT(A)
 #define TRACE(...)
 #endif //NDEBUG
 #define DEBUG_H
