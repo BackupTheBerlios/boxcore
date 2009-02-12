24d5083c8e64b671e8d7f889f544224d880572c2
Carsomyr <carsomyr@users.berlios.de>
2009-02-12 20:51:02 +0200

    Bump the NT version define

--- blackbox/BB.h
+++ blackbox/BB.h
@@ -53,7 +53,7 @@
 /* compiler specifics */
 
 #define WINVER 0x0502
-#define _WIN32_WINNT 0x0500
+#define _WIN32_WINNT 0x0502
 #define _WIN32_IE 0x0501
 #define NO_INTSHCUT_GUIDS
 #define NO_SHDOCVW_GUIDS
