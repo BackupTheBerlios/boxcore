21dacd7b3e46e406a1a4821cbc0b24c3bc18a3c5
Carsomyr <carsomyr@users.berlios.de>
2009-02-18 10:22:35 +0200

    Move some shared functions into a new file

---
+++ blackbox/tasks/helpers.h
@@ -0,0 +1,19 @@
+/*
+ * helpers.h
+ *
+ *  Created on: 14 Feb 2009
+ *      Author: shaun
+ */
+
+#ifndef HELPERS_H_
+#define HELPERS_H_
+
+#include <windows.h>
+
+namespace TaskManagement
+{
+bool IsTask(HWND p_hWnd);
+}
+
+
+#endif /* HELPERS_H_ */
