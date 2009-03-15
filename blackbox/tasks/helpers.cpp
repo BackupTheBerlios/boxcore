149d15c012c4cfd21290dc846e93056a5ece35c9
Carsomyr <carsomyr@users.berlios.de>
2009-03-15 20:56:31 +0200

    Add owner check to IsTask

--- blackbox/tasks/helpers.cpp
+++ blackbox/tasks/helpers.cpp
@@ -24,10 +24,14 @@ bool IsTask(HWND p_hWnd)
 	}
 
 	LONG_PTR exStyle = GetWindowLongPtr(p_hWnd, GWL_EXSTYLE);
-	//HWND owner = GetWindow(p_hWnd, GW_OWNER);
 	if (exStyle & WS_EX_APPWINDOW)
+		{
+			return true;
+		}
+	HWND owner = GetWindow(p_hWnd, GW_OWNER);
+	if (owner)
 	{
-		return true;
+		return false;
 	}
 	if ((exStyle & WS_EX_TOOLWINDOW))
 	{
