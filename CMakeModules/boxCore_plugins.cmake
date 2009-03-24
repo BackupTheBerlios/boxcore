9a374922741e90942e8a4b02c4afd7d1b249a1dc
Carsomyr <carsomyr@users.berlios.de>
2009-03-24 10:23:17 +0200

    Some usefull stuff for stadnalone plugin builds

---
+++ CMakeModules/boxCore_plugins.cmake
@@ -0,0 +1,17 @@
+find_path(BLACKBOX_INCLUDE_DIR "BBApi.h" PATHS "." "../../blackbox" DOC "Location of BBApi.h")
+message (STATUS "Found BBApi.h in ${BLACKBOX_INCLUDE_DIR}")
+
+find_library(BLACKBOX_LIBRARY "blackbox${BOXCORE_EXE_SUFFIX}" PATHS "." "${CMAKE_CURRENT_BINARY_DIR}" "../../blackbox" "../../build/blackbox" DOC "Location of import library for blackbox.exe")
+add_library(blackbox SHARED IMPORTED)
+set_target_properties(blackbox PROPERTIES
+    IMPORTED_IMPLIB "${BLACKBOX_LIBRARY}"
+    IMPORTED_LOCATION "${BLACKBOX_LIBRARY}"   
+)
+message (STATUS "Found Blackbox import library in ${BLACKBOX_LIBRARY}")
+if (BLACKBOX_INCLUDE_DIR STREQUAL "BLACKBOX_INCLUDE_DIR-NOTFOUND" OR BLACKBOX_LIBRARY STREQUAL "BLACKBOX_LIBRARY-NOTFOUND")
+message (FATAL_ERROR "Either the inlude file or link library could not be found. Plese specifiy them manually")
+endif (BLACKBOX_INCLUDE_DIR STREQUAL "BLACKBOX_INCLUDE_DIR-NOTFOUND" OR BLACKBOX_LIBRARY STREQUAL "BLACKBOX_LIBRARY-NOTFOUND")
+
+if (BOXCORE_DEBUG)
+message (FATAL_ERROR "Stadnlone debug builds are not currently supported")
+endif (BOXCORE_DEBUG)  
