e7d96fda5a169723f11a9532366fde184e70e4a4
Carsomyr <carsomyr@users.berlios.de>
2009-04-30 22:17:37 +0200

    Update setup to create ansi boxbar dir as well

--- setup_cross.sh
+++ setup_cross.sh
@@ -3,6 +3,10 @@ mkdir build32
 cd build32
 cmake -DCMAKE_TOOLCHAIN_FILE=../ToolchainFiles/linux-cross-i686-mingw32.cmake ..
 cd ..
+mkdir build32_ansi
+cd build32_ansi
+cmake -DCMAKE_TOOLCHAIN_FILE=../ToolchainFiles/linux-cross-i686-mingw32.cmake ../coreplugins/boxBar
+cd ..
 mkdir build64
 cd build64
 cmake -DCMAKE_TOOLCHAIN_FILE=../ToolchainFiles/linux-cross-x86_64.cmake ..
