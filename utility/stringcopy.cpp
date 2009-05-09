0f069e690c2d3b3353213f0cf49e269cde06d09f
Carsomyr <carsomyr@users.berlios.de>
2009-05-09 20:32:57 +0200

    Update CopyString to allow STL strings as targets

--- utility/stringcopy.cpp
+++ utility/stringcopy.cpp
@@ -7,6 +7,7 @@
 
 #include "stringcopy.h"
 #include <cstdio>
+#include <string.h>
 
 bool CopyString(LPSTR p_dest, LPCSTR p_src, UINT p_max)
 {
@@ -45,5 +46,33 @@ bool CopyString(LPWSTR p_dest, LPCWSTR p_src, UINT p_max)
 {
 	wcsncpy(p_dest, p_src, p_max);
 	p_dest[p_max-1] = L'\0';
+	return true;
+}
+
+bool CopyString(std::basic_string<WCHAR> &p_dest, LPCSTR p_src, UINT p_max)
+{
+	WCHAR buffer[strlen(p_src)+1];
+	MultiByteToWideChar(CP_ACP, 0, p_src, -1, buffer, strlen(p_src)+1);
+	p_dest = buffer;
+	return true;
+}
+
+bool CopyString(std::string &p_dest, LPCSTR p_src, UINT p_max)
+{
+	p_dest = p_src;
+	return true;
+}
+
+bool CopyString(std::basic_string<WCHAR> &p_dest, LPCWSTR p_src, UINT p_max)
+{
+	p_dest = p_src;
+	return true;
+}
+
+bool CopyString(std::string &p_dest, LPCWSTR p_src, UINT p_max)
+{
+	CHAR buffer[2*(wcslen(p_src)+1)];
+	WideCharToMultiByte(CP_ACP, 0, p_src, -1, buffer, wcslen(p_src)+1, NULL, NULL);
+	p_dest = buffer;
 	return true;
 }
--- utility/stringcopy.h
+++ utility/stringcopy.h
@@ -10,10 +10,15 @@
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <string>
 
 bool CopyString(LPSTR p_dest, LPCSTR p_src, UINT p_max);
 bool CopyString(LPSTR p_dest, LPCWSTR p_src, UINT p_max);
 bool CopyString(LPWSTR p_dest, LPCSTR p_src, UINT p_max);
 bool CopyString(LPWSTR p_dest, LPCWSTR p_src, UINT p_max);
+bool CopyString(std::basic_string<WCHAR> &p_dest, LPCSTR p_src, UINT p_max = 0);
+bool CopyString(std::string &p_dest, LPCSTR p_src, UINT p_max = 0);
+bool CopyString(std::basic_string<WCHAR> &p_dest, LPCWSTR p_src, UINT p_max = 0);
+bool CopyString(std::string &p_dest, LPCWSTR p_src, UINT p_max = 0);
 
 #endif /* STRINGCOPY_H_ */
